49b4d7532cc28acc7f73492093b860764585b771
ASK <ASK@DESKTOP-3JJI9D3>
2025-07-06 23:44:43 +0530

    DayOne_Codes

---
+++ DayOne/src/com/DayOne/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.DayOne;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello " + "World " );
+				
+				System.out.println("HelloWorld");
+				
+				System.out.print(20 + 10);
+	}
+
+}
