b25f2663bd6f839b9a3bcbf714942ea5d045ee14
ASK <ASK@DESKTOP-3JJI9D3>
2025-07-17 00:06:06 +0530

    TNS Java Codes

---
+++ AccessModifiers/src/com/accessModifiers/Executor.java
@@ -0,0 +1,13 @@
+package com.accessModifiers;
+
+public class Executor {
+
+	public static void main(String[] args) {
+Base b = new Base();
+		
+		b.methodDefault();
+		b.methodProtected();
+		b.methodPublic();
+	}
+
+}
