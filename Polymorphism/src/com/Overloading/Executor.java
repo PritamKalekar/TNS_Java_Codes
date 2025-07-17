96768033a1f0694a59dc72426d32493dcf92ef36
ASK <ASK@DESKTOP-3JJI9D3>
2025-07-18 00:37:11 +0530

    TNS_Java_Codes

---
+++ Polymorphism/src/com/Overloading/Executor.java
@@ -0,0 +1,14 @@
+package com.Overloading;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		System.out.println("Is 121 palindrome?"+ Operations.isPalindrome(121));
+		System.out.println("Is 123 palindrome?"+ Operations.isPalindrome(123));
+
+		System.out.println("Is CAR Palindrome?"+Operations.isPalindrome("CAR"));
+		System.out.println("Is RACECAR Palindrome?"+Operations.isPalindrome("RACECAR"));	
+			
+			}
+
+}
---
+++ Polymorphism/src/com/Overriding/Shape.java
@@ -0,0 +1,14 @@
+package com.Overriding;
+
+public class Shape {
+
+	public void draw()
+	{
+		System.out.println("Drawing a generic shape");
+	}
+	
+	public void erase()
+	{
+		System.out.println("Erasing a generic shape");
+	}
+}
